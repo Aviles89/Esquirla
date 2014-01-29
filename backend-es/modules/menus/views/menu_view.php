8c06fc8f0bda4b49439062b9a0077be2ed9d82f3
Emmanuel <emmanuel@navegantes.mx>
2014-01-28 20:48:15 -0600

    Preparando version

--- backend-es/modules/menus/views/menu_view.php
+++
@@ -1,9 +0,0 @@
-<div class="mainnav">
-    <ul>
-    	<?php
-        nav_menu_item("about","Inicio", $seccion);
-        nav_menu_item("sliders","Slider", $seccion);
-        nav_menu_item("faq","Faqs", $seccion);
-    	?>
-    </ul>
-</div>
--- backend-es/views/seo/mensajes.php
+++
@@ -1,7 +0,0 @@
-<div class="grid_7" id="acciones">
-  <a href="usuarios" class="grey-button pcb" ><span >REGRESAR</span></a>
-
-</div>
-<div class="grid_7" id="nuevo">
-<?php echo $mensajes;?>
-</div>
